9e45cf77114bbed0face7e9c6263daf5a4a28c43
rnix <rnix@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-17 09:35:50 +0000

    convenience imports in __init__

--- src/cornerstone/browser/__init__.py
+++ src/cornerstone/browser/__init__.py
@@ -1,3 +1,16 @@
+from base import RequestMixin
+from base import RequestTool
+from base import XBrowserView
+from base import AjaxBrowserView
+
+from menu import MenuBase
+from menu import MenuItemBase
+
+from renderer import RendererBase
+
+from tmpl import SelectionVocabBase
+from tmpl import HTMLRendererMixin
+
 from Products.CMFCore import DirectoryView
 DirectoryView.registerDirectory('cornerstone_browser', globals())
 
